9c002c1bcba2d1deeb05e83cdc7524047bacecd9
ksavin <borys.palka@codete.com>
2023-10-05 21:19:51 +0200

    fix for test case 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,53 @@
-export default function Home() {
+import { type ProductItemType } from "@/types";
+import { ProductList } from "@/ui/organisms/ProductList";
+
+const products: ProductItemType[] = [
+	{
+		id: "1",
+		category: "Accessories",
+		name: "Grey T-shirt",
+		price: 35,
+		coverImg: {
+			alt: "Brown leather wallet",
+			src: "/tshirt-1.png",
+		},
+	},
+	{
+		id: "2",
+		category: "Accessories",
+		name: "Leather Wallet",
+		price: 35,
+		coverImg: {
+			alt: "Brown leather wallet",
+			src: "/tshirt-2.png",
+		},
+	},
+	{
+		id: "3",
+		category: "Accessories",
+		name: "Leather Wallet",
+		price: 35,
+		coverImg: {
+			alt: "Brown leather wallet",
+			src: "/tshirt-3.png",
+		},
+	},
+	{
+		id: "4",
+		category: "Accessories",
+		name: "Leather Wallet",
+		price: 35,
+		coverImg: {
+			alt: "Brown leather wallet",
+			src: "/wallet.png",
+		},
+	},
+];
+
+export default function ProductsPage() {
 	return (
 		<section className="flex min-h-screen justify-between p-24">
-			Hello World
+			<ProductList products={products} />
 		</section>
 	);
 }
